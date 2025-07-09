1c80e04fc94133ca5c1264b138a7a42bcb474dde
camilo150 <camicastrillon2314@gmail.com>
2025-07-08 23:53:06 -0500

    f

--- comportamiento/Tienda.js
+++ comportamiento/Tienda.js
@@ -1,8 +1,9 @@
 
 
-function ventanaProductos(producto, prueba){
+function ventanaProductos(botonCerrar,producto, prueba){
 
 document.getElementById(producto).addEventListener("click", function(){document.getElementById(prueba).style.display="contents"});
+document.getElementById(botonCerrar).addEventListener("click", function(){document.getElementById(prueba).style.display="none"});
 }
-ventanaProductos("producto1", "prueba1");
+ventanaProductos("botonCerrar","producto1", "prueba1");
 ventanaProductos("producto2","prueba2");
