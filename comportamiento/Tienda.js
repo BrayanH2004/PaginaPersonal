4fcbdea54e0aae1f4e0da4a3034d3d48feb9083e
Brayan <bhq5678@gmail.com>
2025-07-13 09:01:00 -0500

    BtnCerrar

--- comportamiento/Tienda.js
+++ comportamiento/Tienda.js
@@ -1,9 +1,10 @@
+const btnCerrar =document.getElementById("botonCerrar");
 
+    
 
-function ventanaProductos(botonCerrar,producto, prueba){
-
+function ventanaProductos(producto, prueba){
 document.getElementById(producto).addEventListener("click", function(){document.getElementById(prueba).style.display="contents"});
-document.getElementById(botonCerrar).addEventListener("click", function(){document.getElementById(prueba).style.display="none"});
 }
+btnCerrar.addEventListener('click', function(){document.getElementById('prueba1').style.display='none'});
 ventanaProductos("botonCerrar","producto1", "prueba1");
 ventanaProductos("producto2","prueba2");
